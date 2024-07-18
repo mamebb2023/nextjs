cf3456dc1d26b24dc143795c05cef9003bee2c1b
Mohammednur <mamebb2023@gmail.com>
2024-07-18 14:02:28 +0300

    chore: Update Home page with "All Questions" heading and "Ask a Question" button

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,3 +1,20 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <h1 className="h1-bold text-dark100_light900 font-bold">
+          All Questions
+        </h1>
+        <Link href="/ask-question">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+    </>
+  );
 }
