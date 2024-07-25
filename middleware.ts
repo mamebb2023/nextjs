9f72a1c9bb5ac6c4a38e6d9c26fba4f1d7835b7d
Mohammednur <mamebb2023@gmail.com>
2024-07-25 21:20:03 +0300

    chore: Update middleware to exclude /api/webhook route from protection

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
+// Make sure that the `/api/webhook` route is not protected here
 export default clerkMiddleware()
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Always run for API routes except /api/webhook
+    '/api((?!/webhook).*)',
+    '/trpc(.*)',
   ],
 };
