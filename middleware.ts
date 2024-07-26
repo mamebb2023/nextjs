2fbb96fe5960e4f04c5c8c34e1943354ce6a7a97
Mohammednur <mamebb2023@gmail.com>
2024-07-26 09:32:37 +0300

    chore: Remove console.log statements from code

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,21 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher(['/dashboard']);
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+]);
 
-const middleware = clerkMiddleware((auth, req) => {
-  // Exclude /api/webhook from protection
-  if (req.nextUrl.pathname !== '/api/webhook' && isProtectedRoute(req)) {
-    auth();
-  }
-});
-
-export default middleware;
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+}, { debug: true });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes except /api/webhook
-    '/api((?!/webhook).*)',
-    '/trpc(.*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 };
