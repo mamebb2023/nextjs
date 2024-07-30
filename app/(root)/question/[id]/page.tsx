25ddb3c660213a4baf04a26154f2765a3c6232d2
Mohammednur <mamebb2023@gmail.com>
2024-07-30 11:22:08 +0300

    chore: Update Page component to include type annotation for searchParams

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Page = async ({ params, searchParams }) => {
+const Page = async ({ params, searchParams }: any) => {
   const result = await getQuestionById({ questionId: params.id });
   console.log(result);
   return (
